03807193463f1b75fa3c59dc245a0e1fa157b580
ferdibaum <ferdi99@live.de>
2022-02-04 03:16:11 +0100

    start game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,3 +1,37 @@
+import { useContext } from "react";
+import { RoomContext } from "../contexts/roomContext";
+import { getUserFromLocalStorage } from "../utils/getUserNameFromLocalStorage";
+import { supabase } from "./../supabaseClient";
+
 export function Game() {
-  return <div className="flex mx-3 border rounded-lg grow"></div>;
+  const { room } = useContext(RoomContext);
+
+  async function startGame() {
+    await supabase
+      .from("rooms")
+      .update({
+        gameData: {},
+        game_started: true,
+      })
+      .match({ id: room.id });
+  }
+
+  return (
+    <div className="flex items-center justify-center mx-3 border rounded-lg grow">
+      {!room.game_started ? (
+        room.owner === getUserFromLocalStorage() ? (
+          <div
+            className="p-3 text-white text-center bg-orange-600 rounded-lg cursor-pointer bg-opacity-80 min-w-[150px]"
+            onClick={startGame}
+          >
+            Start Game
+          </div>
+        ) : (
+          <div>Wait for owner to start game.</div>
+        )
+      ) : (
+        <div> Game has Started</div>
+      )}
+    </div>
+  );
 }
